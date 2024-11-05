b804bd87166afb4f0fd2b019fd7eddd3c8f2a2b6
xianrosario <202211990@gordoncollege.edu.ph>
2024-11-05 11:18:42 +0800

    Install google font and tailwind

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import "../app.css";
+</script>
+
+<svelte:head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
+<link href="https://fonts.googleapis.com/css2?family=Afacad+Flux:wght@100..1000&family=Afacad:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
+</svelte:head>
+
+<slot></slot>
